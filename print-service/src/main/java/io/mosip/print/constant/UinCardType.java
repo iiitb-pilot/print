49cb77c39684a8e7984dd99fadfbe745e30383e6
trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
2022-05-17 16:26:18 +0530

    Print Service Development for MEC2

---
+++ print-service/src/main/java/io/mosip/print/constant/UinCardType.java
@@ -0,0 +1,18 @@
+package io.mosip.print.constant;
+
+/**
+ * The Enum UinCardType.
+ * 
+ * @author M1048358 Alok
+ */
+public enum UinCardType {
+
+	/** The pdf. */
+	PDF,
+	/** The text. */
+	TEXT,
+	/** The image. */
+	IMAGE,
+	CARD;
+
+}
