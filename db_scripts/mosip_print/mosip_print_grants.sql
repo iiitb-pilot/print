b6ddddc85e3e600dd594166d0b67cc0be3873ae4
trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
2022-06-11 09:13:58 +0530

    Print Service Modification

--- db_scripts/mosip_print/mosip_print_grants.sql
+++ db_scripts/mosip_print/mosip_print_grants.sql
@@ -7,16 +7,16 @@ GRANT CONNECT
 -- ddl-end --
 
 -- object: grant_3543fb6cf7 | type: PERMISSION --
---GRANT USAGE
---   ON SCHEMA print
---   TO printuser;
+GRANT USAGE
+   ON SCHEMA print
+   TO printuser;
 -- ddl-end --
 
 -- object: grant_8e1a2559ed | type: PERMISSION --
---GRANT SELECT,UPDATE
---   ON ALL TABLES IN SCHEMA print
---   TO printuser;
+GRANT SELECT, INSERT, UPDATE
+   ON ALL TABLES IN SCHEMA print
+   TO printuser;
 -- ddl-end --
 
 --ALTER DEFAULT PRIVILEGES IN SCHEMA print
---	GRANT SELECT,UPDATE ON TABLES TO printuser;
+	GRANT SELECT,UPDATE ON TABLES TO printuser;
